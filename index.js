f5dd37a58d6a3032e12bcbe64a1edba0532d4136
Prashant Kumar <prashantkmr389@gmail.com>
2023-04-01 15:13:55 +0530

    jquery

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const port = 3000;
 
 
 app.use(express.urlencoded());
+app.use(express.static("./assets"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.use("/",require("./routes/"));
+
 
 
 
